60a2a71374884dbdc600f585314a558c135a9465
Milad Nekofar <milad@nekofar.com>
2025-10-05 08:35:07 +0300

    feat: reimplement spinner animation with tailwind

--- packages/ui-web/src/spinner.js
+++ packages/ui-web/src/spinner.js
@@ -1,50 +1,25 @@
-import { css, keyframes } from "@emotion/react";
-
-const rotateAnimation = keyframes({
-  "100%": {
-    transform: "rotate(360deg)",
-  },
-});
-
-const dashAnimation = keyframes({
-  "0%": {
-    strokeDasharray: "90 150",
-    strokeDashoffset: 90,
-  },
-  "50%": {
-    strokeDasharray: "90 150",
-    strokeDashoffset: -45,
-  },
-  "100%": {
-    strokeDasharray: "90 150",
-    strokeDashoffset: -120,
-  },
-});
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 
 const Spinner = ({
   inline = false,
   size,
   color,
   strokeWidth = 6,
+  className,
   style,
   ...props
 }) => (
   <svg
     data-inline={inline || undefined}
     viewBox="0 0 50 50"
-    css={css({
-      width: "var(--size, 2rem)",
-      height: "auto",
-      color: "var(--color, currentColor)",
-      animation: `${rotateAnimation} 2.5s linear infinite`,
-      circle: {
-        animation: `${dashAnimation} 2s ease-in-out infinite`,
-      },
-      "&[data-inline]": {
-        display: "inline-block",
-        width: "0.85em",
-      },
-    })}
+    className={twMerge(
+      clsx(
+        "h-auto w-[var(--size,2rem)] text-[var(--color,currentColor)] animate-spinner-rotate",
+        inline && "inline-block w-[0.85em]",
+      ),
+      className,
+    )}
     style={{ "--size": size, "--color": color, ...style }}
     {...props}
   >
@@ -56,6 +31,7 @@ const Spinner = ({
       stroke="currentColor"
       strokeLinecap="round"
       strokeWidth={strokeWidth}
+      className="animate-spinner-dash"
     />
   </svg>
 );
