6536f34139a968582d0ab62fea4ea7794096677e
Milad Nekofar <milad@nekofar.com>
2025-10-05 08:49:38 +0300

    feat: port emoji component to tailwind

--- packages/ui-web/src/emoji.js
+++ packages/ui-web/src/emoji.js
@@ -1,61 +1,48 @@
 import React from "react";
-import { css } from "@emotion/react";
+import clsx from "clsx";
 import { emoji as emojiUtils } from "@shades/common/utils";
 import { useEmojiById } from "@shades/common/app";
+import { twMerge } from "tailwind-merge";
 
-const Emoji = React.forwardRef(({ large, emoji, children, ...props }, ref) => {
-  const emojiItem = useEmojiById(emoji);
-  const isPictogram = emojiItem?.emoji != null || emojiUtils.isEmoji(emoji);
-  return (
-    <span
-      ref={ref}
-      css={(t) =>
-        css({
-          display: "inline-flex",
-          width: large ? "calc(1.46668em * 1.45)" : "1.46668em",
-          height: large ? "calc(1.46668em * 1.45)" : "1.46668em",
-          overflow: "hidden",
-          verticalAlign: "bottom",
-          "[data-container]": {
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "100%",
-            height: "100%",
-            fontSize: "1.3em",
-          },
-          '&[data-size="large"] [data-container]': {
-            fontSize: "2em",
-          },
-          img: {
-            width: "1em",
-            height: "1em",
-            borderRadius: "0.3rem",
-            objectFit: "cover",
-          },
-          "[data-fallback]": {
-            width: "1em",
-            height: "1em",
-            borderRadius: "0.3rem",
-            background: t.colors.backgroundModifierNormal,
-          },
-        })
-      }
-      data-size={large ? "large" : undefined}
-      {...props}
-    >
-      <span data-container>
-        {isPictogram ? (
-          emoji
-        ) : emojiItem?.url != null ? (
-          <img src={emojiItem.url} loading="lazy" />
-        ) : (
-          <div data-fallback />
+const Emoji = React.forwardRef(
+  ({ large, emoji, children, className, style, ...props }, ref) => {
+    const emojiItem = useEmojiById(emoji);
+    const isPictogram = emojiItem?.emoji != null || emojiUtils.isEmoji(emoji);
+
+    const dimension = large ? "calc(1.46668em * 1.45)" : "1.46668em";
+
+    return (
+      <span
+        ref={ref}
+        data-size={large ? "large" : undefined}
+        className={twMerge(
+          clsx(
+            "group/emoji inline-flex h-[var(--emoji-size)] w-[var(--emoji-size)] overflow-hidden align-bottom",
+          ),
+          className,
         )}
+        style={{ "--emoji-size": dimension, ...style }}
+        {...props}
+      >
+        <span className="flex h-full w-full items-center justify-center text-[1.3em] group-data-[size=large]/emoji:text-[2em]">
+          {isPictogram ? (
+            emoji
+          ) : emojiItem?.url != null ? (
+            <img
+              src={emojiItem.url}
+              loading="lazy"
+              className="h-[1em] w-[1em] rounded-[0.3rem] object-cover"
+            />
+          ) : (
+            <div className="h-[1em] w-[1em] rounded-[0.3rem] bg-(--color-surface-muted)" />
+          )}
+        </span>
+        {children}
       </span>
-      {children}
-    </span>
-  );
-});
+    );
+  },
+);
+
+Emoji.displayName = "Emoji";
 
 export default Emoji;
